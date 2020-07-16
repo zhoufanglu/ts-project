0e014a3885b651365f9d1d90d13e62380ce615de
zhoufanglu <735417031@qq.com>
2020-07-16 09:11:50 +0800

    衍腾pc备份

--- src/views/nodePage/nodePage.vue
+++ src/views/nodePage/nodePage.vue
@@ -10,36 +10,43 @@
     </div>
 </template>
 <script lang="ts">
-  import router from '@/router/index.ts'
-/*  import { Component, Vue } from 'vue-property-decorator';
-  @Component({})
-  export default class About extends Vue {
+    import router from '@/router/index.ts'
+    /*  import { Component, Vue } from 'vue-property-decorator';
+      @Component({})
+      export default class About extends Vue {
 
-  }*/
-  export default {
-    name: '',
-    data() {
-      return {
-        routerList: []
-      }
-    },
-    created() {
-        console.log(27, router)
-      const nodeRouter = router.options.routes.find(i=>{
-      	return i.name === 'nodePage'
-      })
-      this.routerList = nodeRouter.children
-	    let fibonacci: number[] = [1, 1, 2, 3, 5];
-	    console.log(27, fibonacci)
-    },
-    mounted() {
-    },
-    methods: {}
-  }
+      }*/
+    export default {
+        name: '',
+        data() {
+            return {
+                routerList: []
+            }
+        },
+        created() {
+            const routes = (router as any).options.routes
+            console.log(28, routes)
+            const nodeRouter:any = routes.find((
+                i: {
+                    name: string ,
+                    path: string,
+                    component: any }
+                ) => {
+                return i.name === 'nodePage'
+            })
+            console.log(31, nodeRouter)
+           /* this.routerList = nodeRouter.children
+            let fibonacci: number[] = [1, 1, 2, 3, 5];
+            console.log(27, fibonacci)*/
+        },
+        mounted() {
+        },
+        methods: {}
+    }
 </script>
 <style lang="scss" scoped>
     .p-node {
-        h3{
+        h3 {
             cursor: pointer;
         }
     }
